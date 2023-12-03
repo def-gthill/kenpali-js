2d900296a0949d145872a4a10848e738f064c555
Graham Hill <gthill97@gmail.com>
2023-12-03 16:25:11 -0700

    Add .js to imports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { toString } from "./src/builtins";
-import { toJsFunction, toKpFunction } from "./src/interop";
-import kpeval from "./src/kpeval";
-import kpobject from "./src/kpobject";
-import kpparse from "./src/kpparse";
+import { toString } from "./src/builtins.js";
+import { toJsFunction, toKpFunction } from "./src/interop.js";
+import kpeval from "./src/kpeval.js";
+import kpobject from "./src/kpobject.js";
+import kpparse from "./src/kpparse.js";
 
 export { kpeval, kpobject, kpparse, toJsFunction, toKpFunction, toString };
