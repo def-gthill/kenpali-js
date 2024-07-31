9424aee443577d02f539acbb27f4a76bc4d78617
Graham Hill <gthill97@gmail.com>
2024-07-31 12:27:56 -0600

    Add type declaration for matches function

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
-export function kpparse(code: string): object;
 export function kpeval(expression: object, names?: Map<string, any>): any;
-export function toString(value: any): string;
 export function kpobject(...entries: [string, any][]): Map<string, any>;
+export function kpparse(code: string): object;
+export function matches(value: any, schema: any): boolean;
 export function toJsFunction(f: Map<string, any>): function;
 export function toKpFunction(f: function): function;
+export function toString(value: any): string;
