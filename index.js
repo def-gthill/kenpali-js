155d673951b47fd7fc5539f1ea442a9d20ede652
Graham Hill <gthill97@gmail.com>
2024-07-31 12:21:58 -0600

    Export the matches function for validation

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+import { matches } from "./src/bind.js";
 import { toString } from "./src/builtins.js";
 import { toJsFunction, toKpFunction } from "./src/interop.js";
 import kpeval from "./src/kpeval.js";
 import kpobject from "./src/kpobject.js";
 import kpparse from "./src/kpparse.js";
 
-export { kpeval, kpobject, kpparse, toJsFunction, toKpFunction, toString };
+export {
+  kpeval,
+  kpobject,
+  kpparse,
+  matches,
+  toJsFunction,
+  toKpFunction,
+  toString,
+};
