771d4713855ca238640baf2b0eac02b7a27ee158
Graham Hill <gthill97@gmail.com>
2024-11-21 15:58:52 -0700

    Simplify toKpFunction

--- src/interop.js
+++ src/interop.js
@@ -1,8 +1,6 @@
 import { builtin } from "./builtins.js";
-import { kpcatch } from "./kperror.js";
 import { toJsObject, toKpobject } from "./kpobject.js";
 import { kpvmCall } from "./kpvm.js";
-import { isError } from "./values.js";
 
 export function kpcall(kpf, args, namedArgs, { timeLimitSeconds = 0 } = {}) {
   return kpvmCall(kpf, args, toKpobject(namedArgs), { timeLimitSeconds });
@@ -12,18 +10,9 @@ export function toKpFunction(jsf) {
   return builtin(
     jsf.name || "<anonymous>",
     { params: [{ rest: "args" }], namedParams: [{ rest: "namedArgs" }] },
-    (args, namedArgs, kpcallback) => {
-      const result = jsf(
-        args,
-        toJsObject(namedArgs),
-        (callback, args, namedArgs) =>
-          kpcatch(() => kpcallback(callback, args, toKpobject(namedArgs)))
-      );
-      if (isError(result)) {
-        throw result;
-      } else {
-        return result;
-      }
-    }
+    (args, namedArgs, kpcallback) =>
+      jsf(args, toJsObject(namedArgs), (callback, args, namedArgs) =>
+        kpcallback(callback, args, toKpobject(namedArgs))
+      )
   );
 }
