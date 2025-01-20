ad9c725c56f4b5d1239f709340027bfc0a4127cf
Graham Hill <gthill97@gmail.com>
2025-01-19 21:50:13 -0700

    Add explicit stream creation, remove interleave

--- src/builtins.js
+++ src/builtins.js
@@ -403,6 +403,21 @@ const rawBuiltins = [
     }
   ),
   builtin(
+    "newStream",
+    {
+      namedParams: [
+        { name: "value", type: "function" },
+        { name: "next", type: "function" },
+      ],
+    },
+    function ([value, next], kpcallback) {
+      return stream({
+        value: () => kpcallback(value, [], kpobject()),
+        next: () => kpcallback(next, [], kpobject()),
+      });
+    }
+  ),
+  builtin(
     "at",
     {
       params: [
@@ -895,51 +910,6 @@ const rawBuiltins = [
         }
       }
       return streamFrom(start);
-    }
-  ),
-  builtin(
-    "interleave",
-    {
-      params: [
-        { name: "sequences", type: "sequence" },
-        { name: "selector", type: "function" },
-      ],
-    },
-    function ([sequences, selector], kpcallback) {
-      const sequenceArray = toArray(sequences);
-      validateArgument(sequenceArray, arrayOf("sequence"));
-
-      function streamFrom(currents) {
-        if (currents.some((current) => current.isEmpty())) {
-          return emptyStream();
-        } else {
-          function callSelector() {
-            const selectorResult = kpcallback(
-              selector,
-              currents.map((current) => current.value()),
-              kpobject()
-            );
-            validateReturn(selectorResult, "number");
-            return selectorResult;
-          }
-
-          return stream({
-            value() {
-              return indexArray(currents, callSelector()).value();
-            },
-            next() {
-              const selectorResult = callSelector();
-              return streamFrom([
-                ...currents.slice(0, selectorResult - 1),
-                indexArray(currents, selectorResult).next(),
-                ...currents.slice(selectorResult),
-              ]);
-            },
-          });
-        }
-      }
-
-      return streamFrom(sequenceArray);
     }
   ),
   builtin(
