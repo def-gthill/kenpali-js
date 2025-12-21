73fa41bafffeba3fca3d5081386c4c9a960973f8
Graham Hill <gthill97@gmail.com>
2025-12-21 13:42:07 -0700

    Set up lexer module for bootstrapping

--- src/kpparseBootstrap.js
+++ src/kpparseBootstrap.js
@@ -3,14 +3,27 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { kpcall } from "./interop.js";
 import kpeval from "./kpeval.js";
+import kpobject from "./kpobject.js";
 import { kpparseModule } from "./kpparse.js";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default function kpparseBootstrap(code, options = {}) {
+  const lexerModule = loadLexerModule();
+  const parserModule = loadParserModule();
+  const parser = parserModule.find(([name]) => name === "parse")[1];
+  const parse = kpeval(parser, { modules: kpobject(["lexer", lexerModule]) });
+  return kpcall(parse, [code], options);
+}
+
+function loadLexerModule() {
+  const lexerCode = fs.readFileSync(path.join(dirname, "lexer.kpc"));
+  const lexerModule = kpparseModule(lexerCode);
+  return lexerModule;
+}
+
+function loadParserModule() {
   const parserCode = fs.readFileSync(path.join(dirname, "parser.kpc"));
   const parserModule = kpparseModule(parserCode);
-  const parser = parserModule.find(([name]) => name === "parse")[1];
-  const parse = kpeval(parser);
-  return kpcall(parse, [code], options);
+  return parserModule;
 }
