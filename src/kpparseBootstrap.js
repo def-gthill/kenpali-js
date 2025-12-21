68dd4b5f08f0ef05cbfa7f76d42839430be2ac8d
Graham Hill <gthill97@gmail.com>
2025-12-21 13:49:12 -0700

    Set up desugarer module

--- src/kpparseBootstrap.js
+++ src/kpparseBootstrap.js
@@ -10,9 +10,12 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default function kpparseBootstrap(code, options = {}) {
   const lexerModule = loadLexerModule();
+  const desugarerModule = loadDesugarerModule();
   const parserModule = loadParserModule();
   const parser = parserModule.find(([name]) => name === "parse")[1];
-  const parse = kpeval(parser, { modules: kpobject(["lexer", lexerModule]) });
+  const parse = kpeval(parser, {
+    modules: kpobject(["lexer", lexerModule], ["desugarer", desugarerModule]),
+  });
   return kpcall(parse, [code], options);
 }
 
@@ -20,6 +23,12 @@ function loadLexerModule() {
   const lexerCode = fs.readFileSync(path.join(dirname, "lexer.kpc"));
   const lexerModule = kpparseModule(lexerCode);
   return lexerModule;
+}
+
+function loadDesugarerModule() {
+  const desugarerCode = fs.readFileSync(path.join(dirname, "desugarer.kpc"));
+  const desugarerModule = kpparseModule(desugarerCode);
+  return desugarerModule;
 }
 
 function loadParserModule() {
